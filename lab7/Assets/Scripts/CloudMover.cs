ea4e61b44cba8df04a6d4837ccfda1044bd06842
Sanchit <50187427+sskeni@users.noreply.github.com>
2020-05-19 19:17:45 -0700

    Added lab7 files

---
+++ lab7/Assets/Scripts/CloudMover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMover : MonoBehaviour
+{
+    float cloudSpeed = 0.01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + cloudSpeed, transform.position.y, transform.position.z);
+    }
+}
